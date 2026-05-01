66101776511434ca6389c8bcc6d3beea8d7f6b3b
AlejandraMLopez <alejandramelisalopez2@gmail.com>
2026-05-01 10:48:22 -0300

    intento de conexión entre backend y frontend

--- frontend-react/src/api/auth.js
+++ frontend-react/src/api/auth.js
@@ -1,6 +1,4 @@
-import axios from "axios";
-
-const client = axios.create({ baseURL: "http://localhost:8000" });
+import client from "./client";
 
 export const login = async (email, password) => {
   const response = await client.post("/auth/login", { email, password });
