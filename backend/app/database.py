3eaf51e7a8cc5ad9925900401868d51caeb68343
nicolascondemartinez <nicolasconde204@gmail.com>
2026-05-06 17:11:51 -0300

    Actualizo el database corregido

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,10 +6,12 @@ import os
 
 load_dotenv()
 
-load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"})
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
