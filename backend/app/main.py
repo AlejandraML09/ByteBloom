f3abad8af950a32e7a504db500d0440b6d9663b1
Leonel David Gomez <leonel.gomez254008@alumnos.info.unlp.edu.ar>
2026-05-20 18:12:00 -0300

    "fix de archivos duplicados"

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5174",
         "http://localhost:5173",
+        "https://overcrowd-clump-disregard.ngrok-free.dev",
         "https://endereza2.vercel.app"
     ],
     allow_credentials=True,
